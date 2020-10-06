067ca019de780d39580fd497208f3b09604abd1d
Selenophilia <pauloargenal@gmail.com>
2020-10-06 14:49:25 +0800

    Added CRUD for batch model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  get '/batch'          => 'batch#index',  as: 'batch_index'
+  get '/batch/new'      => 'batch#new',    as: 'new_batch'
+  post '/batch'         => 'batch#create', as: 'create_batch'
+  get '/batch/:id'      => 'batch#show',   as: 'show_batch'
+  get '/batch/:id/edit' => 'batch#edit',   as: 'edit_batch'
+  put '/batch/:id'      => 'batch#update', as: 'update_batch'
+
+
 end
