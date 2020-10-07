4a9892f8548c525408b3c7ad20439c589d26cf6a
Selenophilia <pauloargenal@gmail.com>
2020-10-07 16:53:52 +0800

    Modified batch_controller_spec

--- spec/controller/batch_controller_spec.rb
+++ spec/controller/batch_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe BatchController, type: :controller do
 
     login_user
 
-    describe 'GET #index' do
+    describe 'GET batch#index' do
         before do
           get :index
         end
@@ -14,18 +14,57 @@ RSpec.describe BatchController, type: :controller do
         end
     end
 
-    describe 'GET #new' do
+    describe 'GET batch#new' do
         before do
           get :new
         end
-    
-        it 'is expected to render a new template' do
-          is_expected.to render_template(:new)
+        
+        context "when initialized" do
+          subject(:batch) { Batch.new }
+      
+          it "is expected to initialized a new batch" do
+            expect(batch).to be_a_new(Batch)
+          end
+  
+          it 'is expected to render a new template' do
+              is_expected.to render_template(:new)
+          end
         end
     
-      end
+    end
 
-    describe 'GET #edit' do
+    describe "POST batch#create" do
+        context 'when params is valid' do
+
+          it 'create batch with valid attributes' do
+            batch_params = { batch: {batch_name: 'batch1'}}
+            post :create, :params => batch_params
+            expect(response).to have_http_status(302)
+          end
+      end 
+    end
+  
+    describe 'GET batch#show' do
+      before do
+        get :show, params: params
+      end
+  
+      context 'when batch id is valid' do
+        let(:batch) { FactoryBot.create :batch }
+        let(:params) { { id: batch.id } }
+  
+        it 'is expected to set batch instance variable' do
+          expect(assigns[:batch]).to eq(Batch.find_by(id: params[:id]))
+        end
+  
+        it 'is expected to render show template' do
+          is_expected.to render_template(:show)
+        end
+      end
+    end
+
+
+    describe 'GET batch#edit' do
         before do
           get :edit, params: params
         end
@@ -44,23 +83,23 @@ RSpec.describe BatchController, type: :controller do
         end
       end
 
-    describe 'PATCH #update' do
+    describe 'PATCH batch#update' do
 
         before do
-        patch :update, params: params
+           patch :update, params: params
         end
     
         context 'when batch exist in database' do
         let(:batch) { FactoryBot.create :batch }
-        let(:params) { { id: batch.id, batch: { batch: 'batch_name' } } }
+        let(:params) { { id: batch.id } }
 
             context 'when data is provided is valid' do
                 it 'is expected to update batch name' do
-                expect(batch.reload.batch_name).to eq('batch_name')
+                    expect(batch.reload.batch_name).to eq('batch_name')
                 end
 
                 it 'is_expected to redirect_to users_path' do
-                is_expected.to redirect_to(show_batch_url)
+                    is_expected.to redirect_to(show_batch_url)
                 end
             end
         end
