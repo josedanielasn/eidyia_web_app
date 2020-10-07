f6c1dd4083ccef386c27089417e0c37a9a532df3
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-07 19:55:53 +0800

    Added data for user model testing

---
+++ spec/factories/users.rb
@@ -0,0 +1,25 @@
+FactoryBot.define do
+  factory :user do
+    firstname { 'firstname1' }
+    lastname  { 'lastname1' }
+    username  { 'username1' }
+    email {'email@yahoo.com'}
+    role {0}
+    password {'password1234'}
+
+    trait :same_username do
+      username {'username1'}
+    end
+
+    trait :another_uniq_user do
+      firstname { 'firstname2' }
+      lastname  { 'lastname2' }
+      username  { 'username2' }
+      email {'email2@yahoo.com'}
+      role {0}
+      password {'password1234'}
+    end
+
+    factory :another_user_same_username,   traits: [:another_uniq_user, :same_username] 
+  end
+end
