e4765037665dcca01b6704288a985113a38fbd32
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-08 17:26:45 +0800

    Added helper for log_in in devise for testings

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,4 +1,5 @@
 module ControllerMacros
+<<<<<<< HEAD
     def login_user
         before(:each) do
           @request.env["devise.mapping"] = Devise.mappings[:user]
@@ -6,4 +7,13 @@ module ControllerMacros
           sign_in user
         end
     end
+=======
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryBot.create(:user)
+      sign_in user
+    end
+  end
+>>>>>>> d2f6c51... Added helper for log_in in devise for testings
 end
