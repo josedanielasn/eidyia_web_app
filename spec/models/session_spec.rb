fc837848c1334ffec009ea555cd637887e4fdb6f
Selenophilia <pauloargenal@gmail.com>
2020-10-09 14:45:37 +0800

    added new test cases

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,19 +1,14 @@
 RSpec.describe Session, type: :model do
-
     describe 'validations' do
-
         context 'when creating a session' do            
-           
+            subject(:session)  {FactoryBot.create :session}
             it '1.) is expected to validate the presence of session_name ' do
-              session = FactoryBot.create :session
               expect(session).to be_valid
+            end      
+            it "2. is not valid without a session_name" do
+                session.session_name = nil
+                expect(session).to_not be_valid
             end
-            
-            # it "2. is not valid without a session_name" do
-            #     session.session_name = nil
-            #     expect(session).to_not be_valid
-            # end
-
        end
     end  
   end
