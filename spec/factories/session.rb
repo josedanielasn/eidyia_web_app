0b7cefcbf8eb3658e74a6cd2ed29ae057dd6a2c5
Selenophilia <pauloargenal@gmail.com>
2020-10-09 20:53:31 +0800

    Added new trait for session

--- spec/factories/session.rb
+++ spec/factories/session.rb
@@ -1,12 +1,16 @@
 FactoryBot.define do
 
-  factory :session do     
+  factory :session do
+    session_name {'session_name'}
+    program_id {program.id}
+      
       transient do
         program { create(:program) }
       end
-        session_name {'session_name'}
-        program_id {program.id}
-       
+         
+      trait :invalid_session_name do 
+        session_name {''} 
+      end 
   end
 
 end 
