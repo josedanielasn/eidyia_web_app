a0558e8e652486f7aa64b4637dba1606ac904cff
Selenophilia <pauloargenal@gmail.com>
2020-10-10 12:39:13 +0800

    Changed to :description

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,5 @@ class Video < ApplicationRecord
   belongs_to :session
   has_many :comments
   validates :video_title, presence: true, length: {minimum:2, maximum:30}
-  validates :video_title, presence: true, length: {minimum:2, maximum:200}
+  validates :description, presence: true, length: {minimum:2, maximum:200}
 end
