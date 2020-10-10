047c9440e93a429dfe016325a4d753ce4e471655
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-10 09:28:48 +0800

    Added admin trait in calling user factory

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,19 +1,9 @@
 module ControllerMacros
-<<<<<<< HEAD
     def login_user
         before(:each) do
           @request.env["devise.mapping"] = Devise.mappings[:user]
-          user = FactoryBot.create(:user)
+          user = FactoryBot.create(:user, :admin)
           sign_in user
         end
     end
-=======
-  def login_user
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      admin = FactoryBot.create(:user, :admin)
-      sign_in admin
-    end
-  end
->>>>>>> d2f6c51... Added helper for log_in in devise for testings
 end
