5945b1375f6eca35c56e38cbf3ee1319ca8173fe
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-10 12:51:57 +0800

    Added program factory for testing

--- spec/factories/program.rb
+++ spec/factories/program.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
+<<<<<<< HEAD
 
   factory :program do     
       transient do
@@ -10,3 +11,26 @@ FactoryBot.define do
   end
 end 
   
+=======
+  factory :program do
+    transient do
+      course{create(:course)}
+      batch{create(:batch)}
+      course_2{create(:course, :edited_course)}
+      batch_2{create(:batch, :edited_batch)} 
+    end
+     course_id {course.id}
+     batch_id {batch.id}
+
+    trait :invalid_program do
+      course_id {nil}
+      batch_id {nil}
+    end
+
+    trait :edited_program do 
+      course_id {course_2.id}
+      batch_id {batch_2.id}
+    end
+  end
+end
+>>>>>>> a03c92f... Added program factory for testing
