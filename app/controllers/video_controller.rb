bfcaa666e05676c1b649f2ea266957170d6a982c
aeo03 <franzedwarddayrit03@yahoo.com>
2020-10-12 12:40:45 +0800

    added test on video added routes on video added video controller added index in video controller

---
+++ app/controllers/video_controller.rb
@@ -0,0 +1,7 @@
+class VideoController < ApplicationController
+
+  def index
+    @videos = Video.all
+  end
+  
+end
---
+++ spec/factories/videos.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :video do
+      video_title {"video title"}
+      video_description {"video description"}  
+    end
+end 
