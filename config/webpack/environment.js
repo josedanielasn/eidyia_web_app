d1ae02216962dea88511af3d4424c5a0f8111a1a
aeo03 <franzedwarddayrit03@yahoo.com>
2020-10-21 13:06:32 +0800

    Modified webpacker configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,15 +4,9 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
 )
-
-const webpack = require("webpack")
-    environment.plugins.append("Provide", new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-}))
 
 module.exports = environment
